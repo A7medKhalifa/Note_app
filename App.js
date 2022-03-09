5fe60aa9e195845c4ca3198906f4c67635ca8483
A7medKhalifa <87550484+A7medKhalifa@users.noreply.github.com>
2022-03-09 21:47:59 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import NoteNavigation from "./Note App/Navigation/Navigation";
+
+
+export default function App() {
+  return (
+    <>
+        <NoteNavigation />
+    </>
+  )
+}
+
+
+
+
+
+
